62eb4ef565c9651f3bae90426894649dd624b375
jefferson.souza <jefferson.souza@synchro.com.br>
2018-10-04 21:21:55 -0300

    Initial Commit

---
+++ project.clj
@@ -0,0 +1,4 @@
+(defproject learning-clojure "0.1.0-SNAPSHOT"
+  :description "A simple project to learn the basic of clojure language"
+  :url "https://github.com/jmsstudio/learning-clojure"
+  :dependencies [[org.clojure/clojure "1.8.0"]])
---
+++ src/learning_clojure/core.clj
@@ -0,0 +1,8 @@
+(ns learning-clojure.core)
+
+(defn function1
+  "function doc"
+  [x, y]
+  (println x y "Hello, World!"))
+
+(function1 "Test" "sdf")
