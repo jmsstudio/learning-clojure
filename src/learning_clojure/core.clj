99607879623384182194266d01a85db53e8e5b6d
jefferson.souza <jefferson.souza@synchro.com.br>
2018-10-10 19:52:59 -0300

    Testing commands

--- src/learning_clojure/core.clj
+++ src/learning_clojure/core.clj
@@ -6,3 +6,30 @@
   (println x y "Hello, World!"))
 
 (function1 "Test" "sdf")
+
+(defn teste
+  "doc do teste"
+  [a]
+  (println (concat a " bla")))
+
+(teste "123")
+
+(def lista '(1 2 3))
+
+(map #(+ 1 %) lista)
+
+
+(defn double-value [val]
+  (* val 2))
+
+(double-value 15)
+
+(clojure.repl/doc teste)
+
+(let [first-name "John"
+      last-name "Doe"]
+  (print first-name last-name))
+
+(for [x (range 10)]
+  (str "Number: " x))
+
